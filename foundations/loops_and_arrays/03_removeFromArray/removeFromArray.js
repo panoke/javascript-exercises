268b787c6219af0274c94d51ebb9679db1b6e36b
panoke <3214736+panoke@users.noreply.github.com>
2026-09-08 15:04:47 +0100

    Loops and Arrays excercises

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let index;
+
+    for (const remove of toRemove) {
+        do {
+            index = array.indexOf(remove)
+            if (index !== -1) {
+                array.splice(index, 1)
+            }
+        } while (index !== -1)
+    }
+    return array
 };
 
 // Do not edit below this line
--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let sum = 0;
+    if (Math.floor(a) !== a || Math.floor(b) !== b || a < 0 || b < 0) {
+        return 'ERROR'
+    }
+
+    if (a > b) { start = b; end = a;} else {start = a; end = b;}
+
+    for (i = start; i <= end; i++)
+    {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
